ac766c2d951723efa22f2e50fd722e6acbdc68cf
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2026-02-20 00:34:52 -0300

    teste inicial com login do usuário, e limpeza de código

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from bson import ObjectId
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, current_app, jsonify, request
 from pydantic import ValidationError
 
 from app import db
@@ -9,23 +12,34 @@ from app.models.user import LoginPayload
 main_bp = Blueprint("main_bp", __name__)
 
 
-@main_bp.route("/login", methods=["POST"])
+@main_bp.route("/login", methods=["POST"], strict_slashes=False)
 def login():
     try:
         raw_data = request.get_json()
         user_data = LoginPayload(**raw_data)
-
     except ValidationError as e:
         return jsonify({"error": e.errors()}), 400
-
     except Exception as e:
-        return jsonify({"error": "Erro durante a requisição do dado"}), 500
+        return (
+            jsonify({"error": f"Corpo da requisição inválido, ou não é um JSON: {e}"}),
+            500,
+        )
 
     if user_data.username == "admin" and user_data.password == "123":
-        return jsonify({"message": f"Login bem sucedido!"}), 200
-    else:
-        return jsonify({"message": f"Credenciais invalidas"}), 200
-        # return jsonify({"message": f"Realizar login do usuário {user_data.model_dump_json()}."}), 200
+        token = jwt.encode(
+            {
+                "user_id": user_data.username,
+                "exp": datetime.now(timezone.utc) + timedelta(minutes=30),
+            },
+            current_app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+        
+        print(token)
+
+        return jsonify({"access_token": token}), 200
+
+    return jsonify({"message": f"Credenciais invalidas"}), 401
 
 
 @main_bp.route("/")
@@ -40,10 +54,6 @@ def get_products():
         ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True)
         for product in products_cursor
     ]
-
-    # for product in products_cursor:
-    #     product["_id"] = str(product["_id"])
-    #     products_list.append(product)
 
     return jsonify(products_list)
 
@@ -60,7 +70,6 @@ def get_product_by_id(product_id):
 
     product = db.products.find_one({"_id": oid})
     if product:
-        # product["_id"] = str(product["_id"])
         product_model = ProductDBModel(**product).model_dump(
             by_alias=True, exclude_none=True
         )
