1f115a6bcc379b9c76b3a50b7873df858cfe5ac7
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2026-03-01 21:44:03 -0300

    Corrigindo main.py, JÁ QUE NA AULA NÃO ENSINOU CORRETAMENTE AS CORREÇÕES AO ADICIONAR OS TEMPLATES

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,7 +4,17 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 from bson import ObjectId
-from flask import Blueprint, current_app, jsonify, request
+from flask import (
+    Blueprint,
+    current_app,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from pydantic import ValidationError
 
 from app import db
@@ -16,20 +26,36 @@ from app.models.user import LoginPayload
 main_bp = Blueprint("main_bp", __name__)
 
 
+@main_bp.route("/")
+def index():
+    if "jwt_token" not in session:
+        return redirect(url_for("main_bp.login"))
+    return redirect(url_for("main_bp.dashboard"))
+
+
+@main_bp.route("/dashboard")
+def dashboard():
+    if "jwt_token" not in session:
+        return redirect(url_for("main_bp.login"))
+    return render_template("dashboard.html", title="Dashboard")
+
+
+@main_bp.route("/login", methods=["GET"], strict_slashes=False)
+def render_login():
+    return render_template("login.html", title="Login")
+
+
 @main_bp.route("/login", methods=["POST"], strict_slashes=False)
 def login():
-    try:
-        raw_data = request.get_json()
-        user_data = LoginPayload(**raw_data)
-    except ValidationError as e:
-        return jsonify({"error": e.errors()}), 400
-    except Exception as e:
-        return (
-            jsonify({"error": f"Corpo da requisição inválido, ou não é um JSON: {e}"}),
-            500,
-        )
+    # try:
+    # raw_data = request.get_json()
+    username = request.form.get("username")
+    password = request.form.get("password")
+    user_data = LoginPayload(username=username, password=password)
 
-    if user_data.username == "admin" and user_data.password == "123":
+    user_in_db = db.users.find_one({"username": username})
+
+    if user_in_db and user_in_db.get("password") == password:
         token = jwt.encode(
             {
                 "user_id": user_data.username,
@@ -38,41 +64,65 @@ def login():
             current_app.config["SECRET_KEY"],
             algorithm="HS256",
         )
+        session["jwt_token"] = token
 
-        print(token)
+        return redirect(url_for("main_bp.dashboard"))
+    else:
+        flash("Usuário ou senha inválidos", "danger")
+        return redirect(url_for("main_bp.login"))
+    # except ValidationError as e:
+    #     return jsonify({"error": e.errors()}), 400
+    # except Exception as e:
+    #     return (
+    #         jsonify({"error": f"Corpo da requisição inválido, ou não é um JSON: {e}"}),
+    #         500,
+    #     )
 
-        return jsonify({"access_token": token}), 200
+    # if user_data.username == "admin" and user_data.password == "123":
+    #     token = jwt.encode(
+    #         {
+    #             "user_id": user_data.username,
+    #             "exp": datetime.now(timezone.utc) + timedelta(minutes=30),
+    #         },
+    #         current_app.config["SECRET_KEY"],
+    #         algorithm="HS256",
+    #     )
 
-    return jsonify({"message": f"Credenciais invalidas"}), 401
+    #     print(token)
+
+    #     return jsonify({"access_token": token}), 200
+
+    # return jsonify({"message": f"Credenciais invalidas"}), 401
 
 
-@main_bp.route("/")
-def index():
-    return jsonify({"message": "Bem vindo ao StyleSync!"})
+# ----------------------PRODUTOS------------------------
 
 
 @main_bp.route("/products", methods=["GET"], strict_slashes=False)
 def get_products():
-    products_cursor = db.products.find({})
-    products_list = [
-        ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True)
-        for product in products_cursor
-    ]
+    if "jwt_token" not in session:
+        return redirect(url_for("main_bp.login"))
 
-    return jsonify(products_list)
+    products_cursor = db.products.find({})
+    # products_list = [
+    #     ProductDBModel(**product).model_dump(by_alias=True, exclude_none=True)
+    #     for product in products_cursor
+    # ]
+
+    # return jsonify(products_list)
+    return render_template("products.html", products=products_cursor, title="Produtos")
 
 
 @main_bp.route("/products/<string:product_id>", methods=["GET"])
 def get_product_by_id(product_id):
 
     try:
-        oid = ObjectId(str(product_id))
+        oid = ObjectId(product_id)
     except Exception as ex:
-        return jsonify(
-            {"error": f"Erro ao transformar {product_id} em ObjectId: {ex}."}
-        )
+        return jsonify({"error": f"Id do produto inválido"}), 400
 
     product = db.products.find_one({"_id": oid})
+
     if product:
         product_model = ProductDBModel(**product).model_dump(
             by_alias=True, exclude_none=True
@@ -85,32 +135,31 @@ def get_product_by_id(product_id):
 
 @main_bp.route("/products", methods=["POST"], strict_slashes=False)
 @token_required
-def create_product(token):
+def create_product(jwt_token):
     try:
         product = Product(**request.get_json())
+        result = db.products.insert_one(product.model_dump())
+        return (
+            jsonify(
+                {
+                    "message": f"O produto foi criado corretamente!",
+                    "id": str(result.inserted_id),
+                }
+            ),
+            201,
+        )
     except ValidationError as e:
         return jsonify({"error": e.errors()}), 400
-
-    result = db.products.insert_one(product.model_dump())
-    return (
-        jsonify(
-            {
-                "message": f"O produto foi criado corretamente!",
-                "id": str(result.inserted_id),
-            }
-        ),
-        201,
-    )
 
 
 @main_bp.route("/products/<string:product_id>", methods=["PUT"])
 @token_required
-def update_product(token, product_id):
+def update_product(jwt_token, product_id):
     try:
         oid = ObjectId(product_id)
         update_data = UpdateProduct(**request.get_json())
     except ValidationError as e:
-        return jsonify({"error": e.errors()})
+        return jsonify({"error": e.errors()}), 400
 
     update_result = db.products.update_one(
         {"_id": oid}, {"$set": update_data.model_dump(exclude_unset=True)}
@@ -132,8 +181,8 @@ def delete_product(token, product_id):
 
     try:
         oid = ObjectId(product_id)
-    except ValidationError as e:
-        return jsonify({"error": f"ID do produto inválido: {e.errors()}"}), 400
+    except Exception:
+        return jsonify({"error": f"ID do produto inválido"}), 400
 
     delete_result = db.products.delete_one({"_id": oid})
 
@@ -143,9 +192,19 @@ def delete_product(token, product_id):
     return "", 204
 
 
+# ----------------------VENDAS------------------------
+
+
+@main_bp.route("/vendas/upload", methods=["GET"])
+def upload_sales_page():
+    if "jwt_token" not in session:
+        return redirect(url_for("main_bp.login"))
+    return render_template("upload_sales.html", title="Upload de Vendas")
+
+
 @main_bp.route("/sales/upload", methods=["POST"])
 @token_required
-def upload_sales(token):
+def upload_sales(jwt_token):
     if "file" not in request.files:
         return jsonify({"error": "Nenhum arquivo foi enviado"}), 400
 
@@ -166,25 +225,32 @@ def upload_sales(token):
                 sale_data = Sale(**row)
 
                 sales_to_insert.append(sale_data.model_dump())
+
             except ValidationError as e:
-                errors.append(f"Linha {row_num} com dados inválidos")
-            except Exception:
-                errors.append(f"Linha {row_num} com erro inesperado nos dados")
+                errors.append(f"Linha {row_num} com dados inválidos: {e.errors()}")
+            except Exception as e:
+                errors.append(f"Linha {row_num} com erro inesperado: {str(e)}")
 
         if sales_to_insert:
             try:
                 db.sales.insert_many(sales_to_insert)
             except Exception as e:
                 return jsonify({"error": f"{e}"}), 500
-            return (
-                jsonify(
-                    {
-                        "message": "Upload realizado com sucesso",
-                        "vendas_importadas": len(sales_to_insert),
-                        "erros_encontrados": errors,
-                    }
-                ),
-                200,
-            )
 
-    return jsonify({"message": f"Essa é a rota do upload do arquivo de vendas."})
+        return (
+            jsonify(
+                {
+                    "message": "Upload realizado com sucesso",
+                    "vendas_importadas": len(sales_to_insert),
+                    "erros_encontrados": errors,
+                }
+            ),
+            200,
+        )
+
+    return (
+        jsonify(
+            {"error": "Formato de arquivo inválido. Por favor, envie um arquivo .csv"}
+        ),
+        400,
+    )
